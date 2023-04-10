3e7b32235518288a546904c260339b25a35bc3a7
+ <+>
2023-04-10 21:19:21 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Add your code here
+// const formData = {
+//     name: "Steve",
+//     email: "steve@steve.com",
+//   };
+// let name = "Steve"
+// let email = "steve@steve.com"
+  
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: "Steve",
+      email: "steve@steve.com"
+    }),
+  };
+
+function submitData(name, email){
+    return fetch('http://localhost:3000/users', configurationObject)
+    .then(function (response) {
+        return response.json();
+      })
+    .then(function (object) {
+        name = document.getElementById('name');
+        email = document.getElementById('email');
+        name.innerHTML = object.name
+        email.innerHTML = object.email
+      })
+      .catch(function (error) {
+        alert("Bad things! Ragnarők!");
+        console.log(error.message);
+      });
+}
+submitData()
